79dfec73f58888484d572e5be1abb3d3eac14b32
Scott Rhamy <scottrhamy@me.com>
2025-12-03 07:32:47 -0500

    AreaChart Padding fixes (#708)
    * AreaChart Padding fixes
    
    * Remove console.log
    
    ---------
    
    Co-authored-by: Sean Lynch <techniq35@gmail.com>

--- docs/src/examples/components/AreaChart/point-scale.svelte
+++ docs/src/examples/components/AreaChart/point-scale.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
-	import { AreaChart } from 'layerchart';
+	import { AreaChart, defaultChartPadding } from 'layerchart';
 	import { scalePoint } from 'd3-scale';
-	import { createDateSeries, longData } from '$lib/utils/data.js';
+	import { longData } from '$lib/utils/data.js';
 
 	const data = longData.filter((d) => d.year === 2019);
 	export { data };
 </script>
 
-<AreaChart {data} xScale={scalePoint()} x="fruit" y="value" height={300} />
+<AreaChart
+	{data}
+	xScale={scalePoint()}
+	x="fruit"
+	y="value"
+	padding={{ ...defaultChartPadding(), left: 30, right: 15 }}
+	height={300}
+/>
